aadd97aadb76fca3d655e517ed30c1020eced30f
jojomi <jojomi.dev@yahoo.com>
2022-08-03 21:28:26 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR/cmd/bkp" > /dev/null
 rm -f "$(which bkp)"
-GO111MODULE=on go install -v
+go install -v
 which bkp
 popd > /dev/null
